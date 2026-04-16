1c1a768724b9908a0bf00ebe3aeae24524589185
NikitaM <21960067+Keshoid@users.noreply.github.com>
2026-04-17 01:22:30 +0300

    chore: dont print sensitive data in ci script

--- src/node/tests/test_run_net_py/test_nodectl_ci.sh
+++ src/node/tests/test_run_net_py/test_nodectl_ci.sh
@@ -7,5 +7,6 @@ cd "$(dirname "$0")"
 export PARTICIPANTS_WAIT_SECONDS=900   # CI runners are slower
 export NOBUILD=0                       # always rebuild in CI
 export KEEP_NODECTL_ON_SUCCESS=0       # stop everything after test
+export PRINT_SENSITIVE=0               # don't print sensitive data in logs
 
 exec python3 run_singlehost_nodectl.py
